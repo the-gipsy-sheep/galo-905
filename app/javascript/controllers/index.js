ff1bc65c72d12b18ba1d2838439e40bb9c69c2b8
paola pesce <paojpesce@gmail.com>
2022-10-18 20:25:12 -0300

    arrancando con pundit

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+//import Login from "./login"
+//application.register("hello", Login)
