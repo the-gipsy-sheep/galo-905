0c826ffed687f608b96041ebbada9d3eb408e7d1
Gonzalo Cobos Daract <goncobos@gmail.com>
2022-10-18 22:58:49 -0300

    Search Gifts

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -3,8 +3,12 @@ class Gift < ApplicationRecord
   belongs_to :list
   has_many :comments, dependent: :destroy
 
-
   validates :url, :title, presence: true
   validates :title, length: { maximum: 50 }
   validates :description, length: { maximum: 280 }
+
+  include PgSearch::Model
+  pg_search_scope :search_by_gift, against: %i[title price description], using: {
+    tsearch: { prefix: true }
+  }
 end
