b5de7cedc24488ac6c73081734e1b626b09cb48d
Gonzalo Cobos Daract <goncobos@gmail.com>
2022-10-29 15:39:21 -0300

    List en proceso

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -10,3 +10,13 @@ import "../assets/javascript/scroll_animado"
 // import "./plugins"
 
 AOS.init();
+
+
+// FLIP CARDS
+document.querySelectorAll('.flip-card-click .flip-card-inner').forEach(function(item) {
+  item.addEventListener('keypress', function(evt) { if (evt.keyCode == 13 || evt.keyCode == 32) { item.click(); } });
+});
+
+document.querySelectorAll('.flip-card-click').forEach(function(item) {
+  item.addEventListener('click', function () { this.classList.toggle('flipped');  });
+});
